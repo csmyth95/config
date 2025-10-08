12498cdab5056d1b327cf8d7fee1cc5b6fbd2e78
Conor <csmyth95@gmail.com>
2025-10-08 13:43:42 +0100

    Install for NodeJS22 & Claude Code

--- setup.sh
+++ setup.sh
@@ -7,5 +7,21 @@ brew install git
 echo "Setup OhMyZsh for terminal"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "Installing NodeJS 22"
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+# Download and install Node.js:
+nvm install 22
+# Verify the Node.js version:
+node -v # Should print "v22.20.0".
+# Verify npm version:
+npm -v # Should print "10.9.3".
+echo "NodeJS22 Installed"
+
+echo "Installing Claude Code"
+npm install -g @anthropic-ai/claude-code
 
 echo "FINISHED"
